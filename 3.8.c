ce4f8bffcb0b016c44310faed18fa6d669dea476
Adity Bhattacharyya <aditybhattacharyya2005@gmail.com>
2025-09-08 12:32:23 +0530

    Add files via upload

---
+++ 3.8.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+void printList(struct Node* head) {
+    struct Node* current = head;
+
+    while (current != NULL) {
+        printf("%d -> ", current->data);
+        current = current->next;
+    }
+    printf("NULL\n");
+}
+void insertAtBeginning(struct Node** head, int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->next = *head;
+    *head = newNode;
+}
+int main() {
+    struct Node* head = NULL;
+
+    insertAtBeginning(&head, 3);
+    insertAtBeginning(&head, 2);
+    insertAtBeginning(&head, 1);
+
+    printList(head);  
+    return 0;
+}
+
